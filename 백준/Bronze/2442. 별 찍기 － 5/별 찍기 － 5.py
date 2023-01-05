288a2481d1ad75c24dc262a1effa61d9b4e1fe81
slr-09 <101566590+slr-09@users.noreply.github.com>
2023-01-05 22:24:07 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 44 ms, Memory: 30644 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for i in range(n):
+  for j in range(i,n-1):
+    print(" ", end="")
+  for j in range(2*i+1):
+    print("*", end="")
+  print()
+
